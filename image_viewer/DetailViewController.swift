42a301182473a52e8889180ac53fb1c53764f449
Vaughn Antonyan <vaughnanton@gmail.com>
2017-09-28 00:36:07 -0700

    "Working image viewer"

---
+++ image_viewer/DetailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  DetailViewController.swift
+//  image_viewer
+//
+//  Created by Vahag Antonyan on 9/27/17.
+//  Copyright © 2017 Vahag Antonyan. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- image_viewer/ViewController.swift
+++ image_viewer/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     var pictures = [String]()
 
     override func viewDidLoad() {
@@ -24,10 +24,32 @@ class ViewController: UIViewController {
             print(pictures)
         }
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       // try loading the "Detail" view controller and typecasting it to be DetailViewController
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+       
+            // set its selectedImage property
+            vc.selectedImage = pictures[indexPath.row]
+       
+            //push it onto the navigation controller
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
 
 
