ce367e5496047f2a77f31ba976458b6e68571b08
Vaughn Antonyan <vaughnanton@gmail.com>
2017-09-27 20:17:12 -0700

    Initial Commit

---
+++ image_viewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  image_viewer
+//
+//  Created by Vahag Antonyan on 9/27/17.
+//  Copyright © 2017 Vahag Antonyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
