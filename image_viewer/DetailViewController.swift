f40df75b8395358c29ebb940e4eedebd7348cbe9
Vaughn Antonyan <vaughnanton@gmail.com>
2017-09-28 00:41:16 -0700

    "Fix image size and create hiding nav bar"

--- image_viewer/DetailViewController.swift
+++ image_viewer/DetailViewController.swift
@@ -20,6 +20,17 @@ class DetailViewController: UIViewController {
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
         }
+    
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
     }
 
     override func didReceiveMemoryWarning() {
